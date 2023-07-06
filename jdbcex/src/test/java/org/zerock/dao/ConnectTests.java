0aa29db06079295f8bd604f3ab37ed38902738ba
Um-SE.Young <djarlen1@naver.com>
2023-07-06 18:00:49 +0900

    230706

---
+++ jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
@@ -0,0 +1,25 @@
+package org.zerock.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectTests {
+
+    @Test
+    public void testConnection() throws Exception {
+
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3307/webdb",
+                "webuser",
+                "webuser");
+
+        Assertions.assertNotNull(connection);
+
+        connection.close();
+    }
+}
