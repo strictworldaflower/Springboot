4b94fb4970ff5ea7bbbb9adc64501c6d35582f5f
worldzero <129923174+strictworldaflower@users.noreply.github.com>
2023-07-18 17:15:35 +0900

    Add files via upload

---
+++ spring/src/main/java/com/example/springex/sample/SampleService.java
@@ -0,0 +1,14 @@
+package com.example.springex.sample;
+
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@ToString
+public class SampleService {
+
+    @Autowired
+    private SampleDAO sampleDAO;
+
+}
